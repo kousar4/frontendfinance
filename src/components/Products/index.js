99c614d7b458c3c07aec89abed4966e010523a6a
kousarnaaz <93119578+kousarnaaz@users.noreply.github.com>
2022-01-06 15:09:03 +0530

    Update index.js

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,14 +1,103 @@
+import {Component} from 'react'
+
+import Cookies from 'js-cookie'
+import Loader from 'react-loader-spinner'
 import Header from '../Header'
+import DisplayCard from '../DisplayCard'
 
 import './index.css'
 
-const Products = () => (
-  <>
-    <Header />
-    <div className="product-sections">
-      <p>products</p>
+const apiStatusConstants = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  inProgress: 'IN_PROGRESS',
+}
+
+class Products extends Component {
+  state = {
+    postList: [],
+    apiStatus: apiStatusConstants.initial,
+  }
+
+  componentDidMount() {
+    this.getPost()
+  }
+
+  getPost = async () => {
+    this.setState({
+      apiStatus: apiStatusConstants.inProgress,
+    })
+
+    const jwtToken = Cookies.get('jwt_token')
+
+    const apiUrl = 'https://apis.ccbp.in/prime-deals'
+    const options = {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+      method: 'GET',
+    }
+    const response = await fetch(apiUrl, options)
+    if (response.ok === true) {
+      const fetchedData = await response.json()
+      const updatedData = fetchedData.prime_deals.map(product => ({
+        userId: product.userId,
+        id: product.id,
+        title: product.title,
+        body: product.body,
+      }))
+      this.setState({
+        postList: updatedData,
+        apiStatus: apiStatusConstants.success,
+      })
+    } else if (response.status === 401) {
+      this.setState({
+        apiStatus: apiStatusConstants.failure,
+      })
+    }
+  }
+
+  renderPostList = () => {
+    const {postList} = this.state
+    return (
+      <div>
+        <Header />
+        <h1 className="primedeals-list-heading">Exclusive Prime Deals</h1>
+        <ul className="products-list">
+          {postList.map(product => (
+            <DisplayCard displayData={product} key={product.id} />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+  renderFailureView = () => (
+    <>
+      <p className="primedeals-list-heading">Not Found</p>
+    </>
+  )
+
+  renderLoadingView = () => (
+    <div className="products-loader-container">
+      <Loader type="ThreeDots" color="#0b69ff" height="50" width="50" />
     </div>
-  </>
-)
+  )
+
+  render() {
+    const {apiStatus} = this.state
+    switch (apiStatus) {
+      case apiStatusConstants.success:
+        return this.renderPostList()
+      case apiStatusConstants.failure:
+        return this.renderFailureView()
+      case apiStatusConstants.inProgress:
+        return this.renderLoadingView()
+      default:
+        return null
+    }
+  }
+}
 
 export default Products
